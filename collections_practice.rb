fa864bfb93fd667ca4528a891a26b239ca2b7090
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-28 23:30:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,3 +18,35 @@ def swap_elements(array)
   array[1], array[2] = array[2], array[1]
 array
 end
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$" 
+  end 
+end 
+
+def find_a(array)
+  array.find_all do |word| 
+    word[0] == "a"
+  end 
+end 
+
+def sum_array(array)
+  sum = 0 
+  array.each do |num|
+    sum+=num 
+  end 
+end
+
+def add_s(array)
+  array.each do |word|
+    word
+  else
+    word  "s"
+  end
+end
+end 
