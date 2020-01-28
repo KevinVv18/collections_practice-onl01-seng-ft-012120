ef16d0b1a72f198b8727a1eebdb7fb812624ffed
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-28 21:56:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,3 +8,13 @@ def sort_array_desc(array)
   end
 end
 
+def sort_array_char_count(array)
+  array.sort do |left, rigt | 
+    left.length <+> right.length
+  end 
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+array
+end
